8c93aef03621f89c4740e7c62308dd4ba7f0e1a5
cctxjj <118203524+cctxjj@users.noreply.github.com>
2022-11-16 16:29:23 +0100

    Undid changes

--- src/main/java/me/ctxj/herowars/inventoryutil/api/ModifiedInventory.java
+++ src/main/java/me/ctxj/herowars/inventoryutil/api/ModifiedInventory.java
@@ -14,7 +14,6 @@ public class ModifiedInventory {
 
     /*
     Übernimmt bei der Instanziierung ein bestehendes Inventar. Dieses muss einen Wert haben!
-    Test123
      */
     public ModifiedInventory(Inventory inventory) {
         this.inventory = inventory;
